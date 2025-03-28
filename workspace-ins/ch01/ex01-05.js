d822921267298a8bf4f5b46455e0129caaa2aaa6
Kilyong Jeong <uzoolove@gmail.com>
2025-03-28 16:46:22 +0900

    🔧 ppt 임시파일 제외

--- workspace-ins/ch01/ex01-05.js
+++ workspace-ins/ch01/ex01-05.js
@@ -2,3 +2,42 @@
 변수 선언
 */
 
+var a;
+var b, c;
+var d = 10;
+var e = 100, f = 200;
+
+var user = "하츄핑";
+var user2 = '아자핑';
+
+var message = '아자핑이 "안녕하세요." 라고 말했다.';
+var message = "하츄핑이 속으로 '안녕하세요.' 라고 말했다.";
+var message = "피곤핑이 \"피곤해\" 라고 말했다."; // 이스케이프 문자(\)
+var message = "몰라핑이 \n모른다고 말\t했다.";
+var message = "하루핑아 \\놀자\\고 말했다.";
+
+var 변수1 = '좋아핑';
+var 이름_변수 = '싫어핑';
+
+var $money = 4;
+var $4 = 4;
+
+var pi = 3.14;
+
+var _ = 200;
+var $ = 4;
+
+// var user-name = '안돼핑';
+var userName = '낙타핑';
+var userAge = 30;
+var userGender = "여자";
+var userEmail = 'ping@gmail.com';
+
+
+console.log(user, message);
+
+console.log('ping@gmail.com');
+console.log(userEmail);
+
+userEmail = 'ping@naver.com';
+console.log(userEmail);
