ebbefcbfebee0555bd8fb58ec21828c6323885f6
Kilyong Jeong <uzoolove@gmail.com>
2025-04-01 16:38:09 +0900

    ✨ 완성

--- workspace-ins/ch01/ex01-18-04.js
+++ workspace-ins/ch01/ex01-18-04.js
@@ -35,7 +35,7 @@ const systemManagement = 55; // 5. 정보 시스템 구축 관리
 평균: 59
 최고점: 80
 최저점: 40
-아쉽지만 불합격입니다. 사유: 평균 60점 미달
+아쉽지만 불합격입니다. 사유: 평균 60점 미달(59점)
 
 출력 결과 2: softwareDesign = 30일 경우;
 총점: 285
@@ -46,9 +46,68 @@ const systemManagement = 55; // 5. 정보 시스템 구축 관리
 아쉽지만 불합격입니다. 사유: 과락 40점 미달(30점)
 */
 
-const softwareDesign = 48; // 1. 소프트웨어 설계
+const softwareDesign = 30; // 1. 소프트웨어 설계
 const softwareDevelopment = 80; // 2. 소프트웨어 개발
 const databaseSetup = 50; // 3. 데이터베이스 구축
 const programmingUsage = 70; // 4. 프로그래밍 언어 활용
 const systemManagement = 55; // 5. 정보 시스템 구축 관리
+
+const sum = softwareDesign + softwareDevelopment + databaseSetup + programmingUsage + systemManagement;
+const avg = sum / 5;
+
+console.log('총점:', sum);
+console.log('평균:', avg);
+
+let max = 0;
+let min = 0;
+
+if (softwareDesign > softwareDevelopment && softwareDesign > databaseSetup 
+    && softwareDesign > programmingUsage && softwareDesign > systemManagement) {
+  max = softwareDesign;
+} else if (softwareDevelopment > softwareDesign && softwareDevelopment > databaseSetup 
+  && softwareDevelopment > programmingUsage && softwareDevelopment > systemManagement) {
+  max = softwareDevelopment;
+} else if (databaseSetup > softwareDesign && databaseSetup > softwareDevelopment 
+  && databaseSetup > programmingUsage && databaseSetup > systemManagement) {
+  max = databaseSetup;
+} else if (programmingUsage > softwareDesign && programmingUsage > softwareDevelopment 
+  && programmingUsage > databaseSetup && programmingUsage > systemManagement) {
+  max = programmingUsage;
+} else {
+  max = systemManagement;
+}
+
+min = Math.min(softwareDesign, softwareDevelopment, databaseSetup, programmingUsage, systemManagement);
+
+console.log('최고점:', max);
+console.log('최저점:', min);
+
+
+let minScore = softwareDesign; // 48
+if (softwareDevelopment < minScore) {
+  minScore = softwareDevelopment;
+} 
+if (databaseSetup < minScore) {
+  minScore = databaseSetup;
+} 
+if (programmingUsage < minScore) {
+  minScore = programmingUsage;
+} 
+if (systemManagement < minScore) {
+  minScore = systemManagement;
+} 
+console.log('minScore:', minScore);
+
+
+// if (avg >= 60 && softwareDesign >= 40 && softwareDevelopment >= 40 && databaseSetup >= 40 && programmingUsage >= 40 && systemManagement >= 40) {
+if (avg >= 60 && min >= 40) {
+  console.log('축하합니다. 합격입니다.');
+} else {
+  if (avg < 60) {
+    console.log(`아쉽지만 불합격입니다. 사유: 평균 60점 미달(${ avg }점)`);
+  }
+  if (min < 40) {
+    console.log(`아쉽지만 불합격입니다. 사유: 과락 40점 미달(${ min }점)`);
+  }
+}
 
