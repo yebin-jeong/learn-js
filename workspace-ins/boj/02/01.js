81a5290c38db2f88c11f790768ebb8b517367aca
Kilyong Jeong <uzoolove@gmail.com>
2025-04-01 09:52:57 +0900

    ✨ boj 01-02 추가

--- workspace-ins/boj/02/01.js
+++ workspace-ins/boj/02/01.js
@@ -35,3 +35,14 @@ A와 B가 같은 경우에는 '=='를 출력한다.
 ==
 */
 
+// 입력값 예시: 
+// 10 20
+const fs = require("fs");
+const fileData = fs.readFileSync(0).toString().trim().split(" ");
+// console.log(fileData);
+
+const a = parseInt(fileData[0]); // 10
+const b = parseInt(fileData[1]); // 20
+
+console.log(a > b ? '>' : (a < b ? '<' : '=='));
+
