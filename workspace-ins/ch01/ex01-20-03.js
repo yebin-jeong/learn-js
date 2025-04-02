c658907c5fca1710422d2760ded7637a03ff8b18
Kilyong Jeong <uzoolove@gmail.com>
2025-04-02 16:28:35 +0900

    ✨ while문 실습 추가

---
+++ workspace-ins/ch01/ex01-20-03.js
@@ -0,0 +1,33 @@
+/*
+while 문 사용
+
+첫날에 쌀을 한톨 받습니다.
+둘째날 쌀을 두톨 받습니다.
+세째날 쌀을 네톨 받습니다.
+매일 전날의 두배가 되는 쌀알을 받아서 모으면 몇일을 모아야 쌀알을 천만개 모을수 있을까?
+*/
+
+let day = 0;
+let count = 1;
+let sum = 0;
+
+while (sum < 1000_0000) {
+  day++;
+  sum += count;
+  count = count * 2;
+}
+
+console.log(`${day}일 동안 받았더니 쌀알이 ${sum}개 되었다. 내일은 ${count}개 받을수 있겠군.`);
+
+// 30일 동안 몇개의 쌀알을 모을 수 있는가?
+day = 0;
+count = 1;
+sum = 0;
+
+while (day < 30) {
+  day++;
+  sum += count;
+  count = count * 2;
+}
+
+console.log(`${day}일 동안 받았더니 쌀알이 ${sum}개 되었다. 내일은 ${count}개 받을수 있겠군.`);
--- workspace-ins/ch01/ex01-20.js
+++ workspace-ins/ch01/ex01-20.js
@@ -2,3 +2,21 @@
 반복문 - while문
 */
 
+
+// 0부터 10까지 출력
+let i = 0;
+while (i <= 10) {
+  console.log(i++);
+}
+
+// 1부터 10까지의 모든 수의 합계를 출력
+let sum = 0;
+
+i = 1; // 초기화
+while (i <= 10) { // 조건식
+  sum += i; // 실행할 코드
+  i++; // 증감식
+}
+console.log(i);
+console.log(sum);
+
