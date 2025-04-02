837ce83fb42c2ae90453a62b35232828ca68d7ee
Kilyong Jeong <uzoolove@gmail.com>
2025-04-02 16:28:03 +0900

    ✨ if문의 다양한 사용 예제 추가

---
+++ workspace-ins/ch01/ex01-18-05.js
@@ -0,0 +1,192 @@
+/*
+if문의 다양한 사용 예제
+
+*/
+
+/*
+지정한 연산자와 숫자를 받아서 사칙 연산을 수행하는 프로그램
+연산자는 +, -, * , / 만 입력 가능
+잘못된 연산자를 입력할 경우 "잘못된 입력" 출력
+*/
+let operator = '+';
+let n1 = 10;
+let n2 = 20;
+
+if (operator === '+') {
+  console.log(n1 + n2);
+} else if (operator === '-') {
+  console.log(n1 - n2);
+} else if (operator === '*') {
+  console.log(n1 * n2);
+} else if (operator === '/') {
+  console.log(n1 / n2);
+} else {
+  console.log('잘못된 입력', operator);
+}
+
+// 이도울
+if (operator === "+") {
+  console.log(`${n1}${operator}${n2} = ${n1 + n2}`);
+} else if (operator === "-") {
+  console.log(`${n1}${operator}${n2} = ${n1 - n2}`);
+} else if (operator === "*") {
+  console.log(`${n1}${operator}${n2} = ${n1 * n2}`);
+} else if (operator === "/") {
+  console.log(`${n1}${operator}${n2} = ${n1 / n2}`);
+} else {
+  console.log("잘못된 입력입니다.");
+} 
+
+// 조현수
+let sum = 0;
+if (operator == "+") {
+  sum = n1 + n2;
+} else if (operator == "-") {
+  sum = n1 - n2;
+} else if (operator == "*") {
+  sum = n1 * n2;
+} else if (operator == "/") {
+  sum = n1 / n2;
+  if (n1 === 0 || n2 === 0) {
+    console.log("0으로 나눌수 없습니다.");
+  }
+} else if (
+  !(operator && operator == "-" && operator == "*" && operator == "/")
+) {
+  console.log("잘못된 입력");
+}
+console.log(`${n1} ${operator} ${n2} = ${sum}`);
+
+// 오서현
+let result;
+
+if (operator === "+") {
+  result = n1 + n2;
+  // console.log(`${n1} + ${n2} = ${result}`);
+} else if (operator === "-") {
+  result = n1 - n2;
+  // console.log(`${n1} - ${n2} = ${result}`);
+} else if (operator === "*") {
+  result = n1 * n2;
+  // console.log(`${n1} * ${n2} = ${result}`);
+} else if (operator === "/") {
+  result = n1 / n2;
+  // console.log(`${n1} / ${n2} = ${result}`);
+} else {
+  console.log("잘못된 입력");
+}
+console.log(`${n1} ${operator} ${n2} = ${result}`);
+
+// 김태경
+/*
++
+3
+4
+
+-> toString()후
++\r\n3\r\n4\r\n
+
+-> trim()후
++\r\n3\r\n4
+
+-> split('\n')
+input = ['+\r', '3\r', '4']
+
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+// console.log(input);
+
+operator = input[0].trim();
+
+n1 = 10;
+n2 = 20;
+
+if(operator === "+"){
+    console.log(n1 + n2);
+}
+
+else if(operator == "-"){
+    console.log(n1 - n2);
+}
+
+else if(operator == "*"){
+    console.log(n1 * n2);
+}
+
+else if(operator == "/"){
+    console.log(n1 / n2);
+}
+
+else{
+    console.log("잘못된 입력");
+}
+*/
+
+/*
+1 ~ 7 사이의 숫자를 받아서 해당 요일을 출력하는 프로그램
+1은 월요일, 2는 화요일, ... 7은 일요일
+1 ~ 7 사이의 숫자가 아닐 경우 "잘못된 입력" 출력
+*/
+let day = 7;
+
+if (day === 1) {
+  console.log('월요일');
+} else if (day === 2) {
+  console.log('화요일');
+} else if (day === 3) {
+  console.log('수요일');
+} else if (day === 4) {
+  console.log('목요일');
+} else if (day === 5) {
+  console.log('금요일');
+} else if (day === 6) {
+  console.log('토요일');
+} else if (day === 7) {
+  console.log('일요일');
+} else {
+  console.log('잘못된 입력');
+}
+
+// 김태경
+/*
+day = parseInt(input[1]);
+
+if(day == 1) console.log("월요일");
+else if(day == 2) console.log("화요일");
+else if(day == 3) console.log("수요일");
+else if(day == 4) console.log("목요일");
+else if(day == 5) console.log("금요일");
+else if(day == 6) console.log("토요일");
+else if(day == 7) console.log("일요일");
+else console.log("잘못된 입력");
+*/
+
+/*
+1 ~ 12 사이의 월을 받아서 계절을 출력하는 프로그램
+봄: 4월, 여름: 5 ~ 10, 가을: 11, 겨울: 12 ~ 3
+1 ~ 12 사이의 숫자가 아닐 경우 "잘못된 입력" 출력
+*/
+let month = 4;
+
+if (month === 4) {
+  console.log('봄');
+} else if (month === 5 || month === 6 || month === 7 || month === 8 || month === 9) {
+  console.log('여름');
+} else if (month === 11) {
+  console.log('가을');
+} else if (month === 12 || month === 1 || month === 2 || month === 3) {
+  console.log('겨울');
+} else {
+  console.log('잘못된 입력');
+}
+
+// 김태경
+/*
+month = parseInt(input[2]);
+
+if(month >= 3 && month <= 5) console.log("봄");
+else if(month >= 6 && month <= 8) console.log("여름");
+else if(month >= 9 && month <= 11) console.log("가을");
+else if(month == 12 && month > 0 && month <= 2) console.log("겨울");
+else console.log("잘못된 입력");
+*/
