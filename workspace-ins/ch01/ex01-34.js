b4b46002b273f30ef9d830cd5c0043ee8fcc2cc8
Kilyong Jeong <uzoolove@gmail.com>
2025-04-03 16:57:09 +0900

    ✨ 함수 예제 추가

--- workspace-ins/ch01/ex01-34.js
+++ workspace-ins/ch01/ex01-34.js
@@ -2,5 +2,13 @@
 반환값이 있는 함수 - 토스터
 */
 
+function toaster(time, bread){
+  return time + '초 동안 구워진 ' + bread;
+}
 
+const toast1 = toaster(40, '우유 식빵'); // '40초 동안 구워진 우유 식빵'
+const toast2 = toaster(60);
+
+console.log(toast1);
+console.log(toast2);
 
