c7db1cd73875ee10e0704d068941e9aed4260cc7
Kilyong Jeong <uzoolove@gmail.com>
2025-04-07 15:48:10 +0900

    📝

--- workspace-ins/boj/01/02.js
+++ workspace-ins/boj/01/02.js
@@ -28,6 +28,13 @@ main();
 
 // 입력값 예시: 
 // 10 20
+
+
+/**
+ * 표준 입력장치(콘솔)에서 한 줄로 입력된 두 건의 데이터를 읽어서 숫자로 변환한 후
+ * 객체에 a, b 속성으로 저장하여 반환한다.
+ * @returns {object} a, b 속성에 입력값이 저장된 객체
+ */
 function getData() {
   const fs = require("fs");
   const fileData = fs.readFileSync(0).toString().trim().split(" ");
