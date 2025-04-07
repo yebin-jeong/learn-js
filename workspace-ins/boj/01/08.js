358eb3f4f2d48874d173ecbc688ab8b3361d4b02
Kilyong Jeong <uzoolove@gmail.com>
2025-04-07 16:38:50 +0900

    ✨ 문제 풀이 추가

--- workspace-ins/boj/01/08.js
+++ workspace-ins/boj/01/08.js
@@ -22,3 +22,20 @@ ICPC Bangkok Regional에 참가하기 위해 수완나품 국제공항에 막 �
 1998
 */
 
+function main() {
+  const data = getData();
+  // data에서 값을 꺼내서 문제 해결하는 코드 작성
+  console.log(data - (2541-1998));
+}
+main();
+
+/**
+ * 표준 입력장치(콘솔)에서 한 줄로 입력된 한 건의 데이터를 읽어서 반환한다.
+ * @returns {string|number} 읽은 데이터(숫자일 경우 number로 형변환)
+ */
+function getData() {
+  const fs = require("fs");
+  const fileData = fs.readFileSync(0).toString().trim();
+  const result = isNaN(fileData) ? fileData : Number(fileData);
+  return result;
+}
