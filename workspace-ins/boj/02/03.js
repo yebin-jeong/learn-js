e00973adcc3c251a98fd7ca3c2eeb001c216943b
Kilyong Jeong <uzoolove@gmail.com>
2025-04-08 11:36:27 +0900

    ✨ 02/03 윤년

--- workspace-ins/boj/02/03.js
+++ workspace-ins/boj/02/03.js
@@ -8,7 +8,9 @@
 
 윤년은 연도가 4의 배수이면서, 100의 배수가 아닐 때 또는 400의 배수일 때이다.
 
-예를 들어, 2012년은 4의 배수이면서 100의 배수가 아니라서 윤년이다. 1900년은 100의 배수이고 400의 배수는 아니기 때문에 윤년이 아니다. 하지만, 2000년은 400의 배수이기 때문에 윤년이다.
+예를 들어, 2012년은 4의 배수이면서 100의 배수가 아니라서 윤년이다. 
+1900년은 100의 배수이고 400의 배수는 아니기 때문에 윤년이 아니다. 
+하지만, 2000년은 400의 배수이기 때문에 윤년이다.
 
 입력
 첫째 줄에 연도가 주어진다. 연도는 1보다 크거나 같고, 4000보다 작거나 같은 자연수이다.
@@ -27,9 +29,28 @@
 0
 */
 
-// 입력값 예시: 
-// 10
-const fs = require("fs");
-const fileData = fs.readFileSync(0).toString().trim().split(" ");
+function main() {
+  const data = getData();
+  // data에서 값을 꺼내서 문제 해결하는 코드 작성
+  let result = 0; // 초기값은 윤년이 아님
 
-const year = parseInt(fileData[0]); // 10
+  // 윤년이 되는 경우
+  // 4의 배수 && (100의 배수가 아니거나 400의 배수)
+  if (data % 4 === 0 && (data % 100 !== 0 || data % 400 === 0)) {
+    result = 1;
+  }
+
+  console.log(result);
+}
+main();
+
+/**
+ * 표준 입력장치(콘솔)에서 한 줄로 입력된 한 건의 데이터를 읽어서 반환한다.
+ * @returns {string|number} 읽은 데이터(숫자일 경우 number로 형변환)
+ */
+function getData() {
+  const fs = require("fs");
+  const fileData = fs.readFileSync(0).toString().trim();
+  const result = isNaN(fileData) ? fileData : Number(fileData);
+  return result;
+}
