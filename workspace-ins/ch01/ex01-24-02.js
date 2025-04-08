ed190f497a906b8351c57b4b36f3b02fea4ced5a
Kilyong Jeong <uzoolove@gmail.com>
2025-04-08 16:43:46 +0900

    .

--- workspace-ins/ch01/ex01-24-02.js
+++ workspace-ins/ch01/ex01-24-02.js
@@ -18,21 +18,6 @@ const score = {
   }
 };
 
-const score2 = {
-  kor: 100,
-  eng: 90,
-  math: 85,
-  sum: function(){ // 총점을 반환한다.
-    // TODO this = score
-    // 객체의 메서드가 자신의 속성을 접근할때 this 사용
-    // return this.kor + this.eng + this.math;
-    return score.kor + score.eng + score.math;
-  },
-  avg: function(){ // 평균을 반환한다.
-    return this.sum() / 3;
-  }
-};
-
 console.log(score.kor, score.eng, score['math']);
 console.log('총점', score.sum());
 console.log('평균', score.avg());
