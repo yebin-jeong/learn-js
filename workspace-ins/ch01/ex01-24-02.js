14fbf473ccd41955b5098d7ddef0b4a4420f5786
Kilyong Jeong <uzoolove@gmail.com>
2025-04-08 16:21:08 +0900

    ✨ sum, avg 메서드 추가

--- workspace-ins/ch01/ex01-24-02.js
+++ workspace-ins/ch01/ex01-24-02.js
@@ -4,14 +4,29 @@
 
 // 객체로 만들기
 const score = {
-  userName: '차차핑',
+  kor: 100,
+  eng: 90,
+  math: 85,
+  sum: function(){ // 총점을 반환한다.
+    // TODO this = score, 메서드 내부에서 this가 사용될때 this는 메서드를 가진 객체를 가리킴
+    // 객체의 메서드가 자신의 속성을 접근할때 this 사용
+    // return this.kor + this.eng + this.math;
+    return score.kor + score.eng + score.math;
+  },
+  avg: function(){ // 평균을 반환한다.
+    return this.sum() / 3;
+  }
+};
+
+const score2 = {
   kor: 100,
   eng: 90,
   math: 85,
   sum: function(){ // 총점을 반환한다.
     // TODO this = score
     // 객체의 메서드가 자신의 속성을 접근할때 this 사용
-    return this.kor + this.eng + this.math;
+    // return this.kor + this.eng + this.math;
+    return score.kor + score.eng + score.math;
   },
   avg: function(){ // 평균을 반환한다.
     return this.sum() / 3;
