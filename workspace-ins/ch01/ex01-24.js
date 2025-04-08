b0e40e971778933bfa61efccb6f4545d2fb8669f
Kilyong Jeong <uzoolove@gmail.com>
2025-04-08 16:01:24 +0900

    ✨ JSON

--- workspace-ins/ch01/ex01-24.js
+++ workspace-ins/ch01/ex01-24.js
@@ -1,4 +1,18 @@
 /*
 JSON 표기법 - 리터럴 방식으로 객체 생성
+JavaScript Object Notation
 */
 
+const foo = {
+  name: '제이슨핑',
+  age: 7,
+  job: '요정',
+  married: true
+};
+
+foo.email = 'json@gmail.com';
+
+// 함수핑 6 마법사 false
+console.log(foo['name'], foo['age'], foo.job, foo.married);
+// object { name: '함수핑', age: 6, job: '마법사', married: false }
+console.log(typeof foo, foo);
