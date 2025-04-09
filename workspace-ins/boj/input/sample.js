0d592860cdda0c1041712082e497dff406c83018
Kilyong Jeong <uzoolove@gmail.com>
2025-04-09 17:19:18 +0900

    ✨ 여러건 입력 데이터 추출하는 샘플 추가

--- workspace-ins/boj/input/sample.js
+++ workspace-ins/boj/input/sample.js
@@ -1,24 +1,12 @@
 function main() {
   const data = getData();
   // data에서 값을 꺼내서 문제 해결하는 코드 작성
-  console.log(data);
+  
 }
 main();
 
-
-/*
-23 48
-25
-
-리턴값
-[
-  [23, 48]
-  [25]
-]
-*/
-
 /**
- * 표준 입력장치(콘솔)에서 두 줄로 입력된 줄당 두 건의 데이터를 읽어서 숫자로 변환한 후
+ * 표준 입력장치(콘솔)에서 여러줄로 입력된 줄당 여러 건의 데이터를 읽어서 숫자로 변환한 후
  * 배열로 저장하여 반환한다.
  * @returns {[]} 2차원 배열
  */
@@ -33,10 +21,10 @@ function getData() {
   for (let i=0; i< arr.length; i++) {
     const row = arr[i]; // '23 48', '25'
     const rowArr = row.split(' '); // ['23', '48'], ['25']
+    for (let k=0; k<rowArr.length; k++) {
+      rowArr[k] = isNaN(rowArr[k]) ? rowArr[k] : parseInt(rowArr[k]);
+    }
     result.push(rowArr);
   }
-
-  // result.a = isNaN(fileData[0]) ? fileData[0] : parseInt(fileData[0]);
-  // result.b = isNaN(fileData[1]) ? fileData[1] : parseInt(fileData[1]);
   return result;
 }
