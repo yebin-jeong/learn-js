0798412fe3a698b9f1f6e34727655729c3fee5d6
Kilyong Jeong <uzoolove@gmail.com>
2025-04-09 15:38:36 +0900

    🚧 유사 배열 객체

--- workspace-ins/ch01/ex01-28.js
+++ workspace-ins/ch01/ex01-28.js
@@ -13,9 +13,23 @@ function printArr (arr) {
 }
 
 const colorArr = [ 'orange', 'yellow', 'green' ];
-
+colorArr.push('black');
 printArr(colorArr);
 
-const arr = {};
-
+/*
+TODO 유사 배열 객체
+유사 배열 객체
+  - length 속성
+  - 0부터 시작해서 1씩 증가하는 속성 추가
+*/
+const arr = {
+  length: 3,
+  0: 'orange',
+  1: 'yellow',
+  2: 'green'
+};
+arr.push('black');
 printArr(arr);
+
+// 문자열도 유사 배열 객체의 일종
+// printArr('이것은 문자열');
