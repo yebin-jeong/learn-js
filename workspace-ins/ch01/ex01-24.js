685e76dab7ba009ec23b62897bf040f8fac92bc3
Kilyong Jeong <uzoolove@gmail.com>
2025-04-09 09:47:46 +0900

    👔 JSON.stringify 추가

--- workspace-ins/ch01/ex01-24.js
+++ workspace-ins/ch01/ex01-24.js
@@ -10,6 +10,18 @@ const foo = {
   married: true
 };
 
+// 네트워크로 전송할 때는 꼭 문자열을 큰 따옴표로 감싸야 한다.
+// TODO ajax 단원에서..., JSON.stringify() 함수를 사용하면 객체를 JSON 형태의 문자열로 변환 가능
+const strFoo = JSON.stringify(foo);
+console.log('strFoo', typeof strFoo, strFoo);
+
+// const foo = {
+//   "name": "제이슨핑",
+//   "age": 7,
+//   "job": "요정",
+//   "married": true
+// };
+
 foo.email = 'json@gmail.com';
 
 // 함수핑 6 마법사 false
