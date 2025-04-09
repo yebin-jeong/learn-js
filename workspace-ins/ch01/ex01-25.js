9eaef6715ec4764c5065182aa569ca1c18b7ba1f
Kilyong Jeong <uzoolove@gmail.com>
2025-04-09 10:40:02 +0900

    ✨ 객체의 프로퍼티 다루기

--- workspace-ins/ch01/ex01-25.js
+++ workspace-ins/ch01/ex01-25.js
@@ -2,3 +2,27 @@
 객체의 속성 관리
 */
 
+const foo = {
+  name: '푸푸핑',
+  mainJob: 'magician',
+};
+
+// 객체 프로퍼티 읽기
+console.log(foo.name, foo['mainJob'], foo.subJob);
+
+// 객체 프로퍼티 갱신
+foo.mainJob = 'fairy';
+console.log(foo.name, foo['mainJob'], foo.subJob);
+
+// 객체 프로퍼티 동적 생성
+foo.subJob = 'celeb';
+console.log(foo.name, foo['mainJob'], foo.subJob);
+
+// 객체 프로퍼티 삭제
+delete foo.mainJob;
+console.log(foo.name, foo['mainJob'], foo.subJob);
+
+// 객체의 모든 프로퍼티 읽기
+for (let prop in foo) { // 1) prop='name', 2) prop='subJob'
+  console.log(prop, foo[prop]);
+}
