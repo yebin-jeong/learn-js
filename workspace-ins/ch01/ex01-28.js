bdd6332387f5f9c56aab99013a281191bd213c38
Kilyong Jeong <uzoolove@gmail.com>
2025-04-09 15:09:35 +0900

    🚧 유사 배열 객체

--- workspace-ins/ch01/ex01-28.js
+++ workspace-ins/ch01/ex01-28.js
@@ -2,3 +2,20 @@
 유사 배열 객체
 */
 
+/**
+ * 전달받은 배열의 모든 요소를 인덱스와 함께 출력한다.
+ * @param {any[]} arr - 배열
+ */
+function printArr (arr) {
+  for (let i=0; i<arr.length; i++){
+    console.log(i, arr[i]);
+  }
+}
+
+const colorArr = [ 'orange', 'yellow', 'green' ];
+
+printArr(colorArr);
+
+const arr = {};
+
+printArr(arr);
