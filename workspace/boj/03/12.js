0e24e296c57349ff7ccf7213a049bf73cf67416b
yebin-jeong <2022112390@dgu.ac.kr>
2025-04-09 16:16:36 +0900

    백준 3-12 문제

--- workspace/boj/03/12.js
+++ workspace/boj/03/12.js
@@ -28,3 +28,13 @@
 7
 */
 
+const fs = require("fs");
+const fileData = fs.readFileSync(0).toString().trim().split("\n");
+
+for (let i = 0; i < fileData.length; i++) {
+  const data = fileData[i].split(" ");
+  const a = parseInt(data[0]);
+  const b = parseInt(data[1]);
+
+  console.log(a + b);
+}
