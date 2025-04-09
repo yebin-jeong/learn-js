0745106a0d1c7617f3c71ca39624a4b5be270c48
Kilyong Jeong <uzoolove@gmail.com>
2025-04-09 11:51:16 +0900

    ✨ 배열 예제

--- workspace-ins/ch01/ex01-26.js
+++ workspace-ins/ch01/ex01-26.js
@@ -2,3 +2,19 @@
 배열 생성, 읽기, 쓰기
 */
 
+// const arr = new Array(1, 2, 3); // [ 1, 2, 3 ] 생성
+const arr = [ 1, 2, 3 ];
+// 지정한 데이터를 배열의 마지막 요소로 추가한다.
+arr.push(10); // [ 1, 2, 3, 10 ]
+arr[4] = 20; // [ 1, 2, 3, 10, 20 ]
+arr[5] = 30; // [ 1, 2, 3, 10, 20, 30 ]
+arr[1] = 100; // [ 1, 100, 3, 10, 20, 30 ]
+arr[9] = 200; // [ 1, 100, 3, 10, 20, 30, , , , 200 ]
+// ......
+// 배열의 마지막에 새로운 데이터 추가
+arr[arr.length] = 300;
+arr.push(400); // arr[arr.length] 대신 배열 메서드 push를 사용한게 쉬움
+
+console.log('배열의 길이', arr.length);
+console.log('배열의 마지막 값', arr[arr.length-1]);
+console.log(typeof arr, arr);
