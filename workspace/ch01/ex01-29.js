0933f1241074589288311507dee493ff618133b3
yebin-jeong <2022112390@dgu.ac.kr>
2025-04-09 16:36:14 +0900

    이차배열

--- workspace/ch01/ex01-29.js
+++ workspace/ch01/ex01-29.js
@@ -2,5 +2,20 @@
 2차원 배열
 */
 
+const numbers = [
+  [2, 4, 6, 8],
+  [3, 6, 9, 12],
+  [4, 8, 12, 16],
+  [5, 10, 15, 20],
+];
 
+console.log(numbers[3][2]);
 
+let sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+  for (let k = 0; k < numbers[i].length; k++) {
+    console.log(numbers[i][k]);
+    sum += numbers[i][k];
+  }
+}
+console.log(sum);
