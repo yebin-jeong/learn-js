4d7abfd45c5c8d9cfd3d10bb00a8f4a396d9404b
Kilyong Jeong <uzoolove@gmail.com>
2025-04-11 08:27:46 +0900

    ♻️ 각 파일에 상세한 주석 추가

--- workspace-ins/ch01/ex01-29-02.js
+++ workspace-ins/ch01/ex01-29-02.js
@@ -1,67 +1,36 @@
 /*
-2차원 배열을 사용한 라이언 수강생 관리 시스템
-각 반의 학생들을 2차원 배열로 관리하고, 전체 수강생을 조회하는 예제
+2차원 배열
+FEBC 수강생의 이름을 저장하는 배열을 작성해 보자.
 */
 
-// 각 반의 학생들
+const students13 = ['임한길', '이호정', '김현지', '이도울', '박선영', '김하영'];
+const students12 = ['십이기핑', '몰라핑', '안가르쳤어핑'];
+
 const febcStudents = [
-  '하츄핑', '아자핑', '차차핑', '바로핑', '라라핑'
+  ['십이기핑', '몰라핑', '안가르쳤어핑'],
+  ['임한길', '이호정', '김현지', '이도울', '박선영', '김하영']
 ];
 
 const bebcStudents = [
-  '해핑', '키키핑', '아잉핑', '부끄핑', '부투핑'
+  ['십이기핑', '몰라핑', '안가르쳤어핑'],
+  ['임한길', '이호정', '김현지', '이도울', '박선영', '김하영']
 ];
 
 const aosbcStudents = [
-  '깜빡핑', '띠용핑', '주르핑', '차나핑', '따라핑', '무셔핑'
+  ['십이기핑', '몰라핑', '안가르쳤어핑'],
+  ['임한길', '이호정', '김현지', '이도울', '박선영', '김하영']
 ];
 
 const iosbcStudents = [
-  '나르핑', '투투핑', '차캐핑', '떠벌핑'
+  ['십이기핑', '몰라핑', '안가르쳤어핑'],
+  ['임한길', '이호정', '김현지', '이도울', '박선영', '김하영']
 ];
 
-// 전체 멋사 수강생 (2차원 배열)
 const lionStudents = [
-  febcStudents,  // FEBC 수강생
-  bebcStudents,  // BEBC 수강생
-  aosbcStudents, // AOSBC 수강생
-  iosbcStudents  // IOSBC 수강생
+  febcStudents,
+  bebcStudents,
+  aosbcStudents,
+  iosbcStudents
 ];
 
-// 각 반의 학생 수 출력
-console.log('FEBC 학생 수:', febcStudents.length);
-console.log('BEBC 학생 수:', bebcStudents.length);
-console.log('AOSBC 학생 수:', aosbcStudents.length);
-console.log('IOSBC 학생 수:', iosbcStudents.length);
-
-// 전체 멋사 수강생 수 계산
-let totalStudents = 0;
-for (let i = 0; i < lionStudents.length; i++) {
-  for (let j = 0; j < lionStudents[i].length; j++) {
-    totalStudents++;
-  }
-}
-// const totalStudents = lionStudents.reduce((total, classStudents) => total + classStudents.length, 0);
-console.log(`총 멋사 수강생 수: ${totalStudents}`);
-
-// 특정 반의 모든 학생 출력
-const classIndex = 0; // FEBC
-console.log('FEBC 학생들');
-for (let i = 0; i < lionStudents[classIndex].length; i++) {
-  console.log(`${lionStudents[classIndex][i]}`);
-}
-// console.log(`FEBC 학생들: ${lionStudents[classIndex].join(', ')}`);
-
-// 특정 학생이 있는지 확인
-const studentToFind = '주르핑';
-
-// 2차원 배열 조회
-for (let i = 0; i < lionStudents.length; i++) {
-  for (let j = 0; j < lionStudents[i].length; j++) {
-    if (lionStudents[i][j] === studentToFind) {
-      console.log(`${studentToFind} 학생이 있습니다.`);
-      break;
-    }
-  }
-}
 
