9bace659f87677a10b7cac4e61761bbbb117d452
Kilyong Jeong <uzoolove@gmail.com>
2025-04-10 15:07:49 +0900

    03/12 A+B EOF

--- workspace-ins/boj/03/12.js
+++ workspace-ins/boj/03/12.js
@@ -28,12 +28,24 @@
 7
 */
 
-const fs = require("fs");
-const fileData = fs.readFileSync(0).toString().trim().split("\n");
-
-for (let i = 0; i < fileData.length; i++) {
-  const data = fileData[i].split(" "); // i번째 줄에서 스페이스로 나누고 
-  const A = parseInt(data[0]); // i줄에서 첫번째로 받는 값이 A
-  const B = parseInt(data[1]); // i줄에서 두번째로 받는 값이 B
+function main() {
+  const data = getData();
+  // console.log(data);
   
+  for(let i=0; i<data.length; i++){
+    console.log(data[i][0] + data[i][1]);
+  }
 }
+
+function getData() {
+  const arr = require("fs").readFileSync(0).toString().trim().split("\n");
+  const result = [];
+  for (let row of arr) {
+    const rowArr = row.split(' ');
+    for (let k=0; k<rowArr.length; k++) rowArr[k] = isNaN(rowArr[k]) ? rowArr[k] : parseInt(rowArr[k]);
+    result.push(rowArr.length === 1 ? rowArr[0] : rowArr);
+  }
+  return result.length===1 ? result[0] : result;
+}
+
+main();
