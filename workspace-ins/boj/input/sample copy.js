dadc8f6a0fdda9befeec2e170e7c2b58fa70cd72
Kilyong Jeong <uzoolove@gmail.com>
2025-04-10 09:33:23 +0900

    ♻️ 백준 제출용

---
+++ workspace-ins/boj/input/sample copy.js
@@ -0,0 +1,18 @@
+function main() {
+  const data = getData();
+  console.log(data);
+}
+main();
+
+function getData() {
+  const fs = require("fs");
+  const fileData = fs.readFileSync(0).toString();
+  const arr = fileData.trim().split("\n");
+  const result = [];
+  for (let row of arr) {
+    const rowArr = row.split(' ');
+    for (let k=0; k<rowArr.length; k++) rowArr[k] = isNaN(rowArr[k]) ? rowArr[k] : parseInt(rowArr[k]);
+    result.push(rowArr);
+  }
+  return result;
+}
