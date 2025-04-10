d9d3ab7c72b416fde07bfbc820d71a9a06f48aa7
Kilyong Jeong <uzoolove@gmail.com>
2025-04-10 09:28:04 +0900

    👔 sample for-of로 수정

--- workspace-ins/boj/input/sample.js
+++ workspace-ins/boj/input/sample.js
@@ -1,7 +1,7 @@
 function main() {
   const data = getData();
   // data에서 값을 꺼내서 문제 해결하는 코드 작성
-  
+  console.log(data);
 }
 main();
 
@@ -18,8 +18,16 @@ function getData() {
   const arr = fileData.trim().split("\n");
 
   const result = []; // 리턴할 2차원 배열
-  for (let i=0; i< arr.length; i++) {
-    const row = arr[i]; // '23 48', '25'
+  // for (let i=0; i< arr.length; i++) {
+  //   const row = arr[i]; // '23 48', '25'
+  //   const rowArr = row.split(' '); // ['23', '48'], ['25']
+  //   for (let k=0; k<rowArr.length; k++) {
+  //     rowArr[k] = isNaN(rowArr[k]) ? rowArr[k] : parseInt(rowArr[k]);
+  //   }
+  //   result.push(rowArr);
+  // }
+
+  for (let row of arr) { // '23 48', '25'
     const rowArr = row.split(' '); // ['23', '48'], ['25']
     for (let k=0; k<rowArr.length; k++) {
       rowArr[k] = isNaN(rowArr[k]) ? rowArr[k] : parseInt(rowArr[k]);
