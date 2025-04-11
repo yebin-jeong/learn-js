d24b06204dc65bd56b717f08fff13c8dd82b5b91
yebin-jeong <2022112390@dgu.ac.kr>
2025-04-11 10:03:06 +0900

    백준 4-5 공 넣기

--- workspace/boj/04/05.js
+++ workspace/boj/04/05.js
@@ -36,3 +36,36 @@
 1 2 1 1 0
 */
 
+function main() {
+  const data = getData();
+  // console.log(data);
+  const n = data[0][0];
+  let result = new Array(n).fill(0);
+
+  for (let i = 1; i < data.length; i++) {
+    const rowArr = data[i];
+    const x = rowArr[0];
+    const y = rowArr[1];
+    const z = rowArr[2];
+
+    for (let k = x; k <= y; k++) {
+      result[k - 1] = z;
+    }
+  }
+
+  console.log(result.join(" "));
+}
+
+function getData() {
+  const arr = require("fs").readFileSync(0).toString().trim().split("\n");
+  const result = [];
+  for (let row of arr) {
+    const rowArr = row.split(" ");
+    for (let k = 0; k < rowArr.length; k++)
+      rowArr[k] = isNaN(rowArr[k]) ? rowArr[k] : parseInt(rowArr[k]);
+    result.push(rowArr.length === 1 ? rowArr[0] : rowArr);
+  }
+  return result.length === 1 ? result[0] : result;
+}
+
+main();
