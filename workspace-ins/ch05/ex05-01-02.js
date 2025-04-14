8d9541ed27f438f74cde0a2154fb5b3a3ec19923
Kilyong Jeong <uzoolove@gmail.com>
2025-04-14 15:10:44 +0900

    🚧  DOM API 사용 예제

---
+++ workspace-ins/ch05/ex05-01-02.js
@@ -0,0 +1,6 @@
+/*
+첫번째 구매 항목 출력(두부)하고 그 값을 양파로 수정
+document.getElementById(id): id 속성으로 요소 선택
+텍스트 노드의 nodeValue: 문자열 값
+*/
+
---
+++ workspace-ins/ch05/ex05-01-03.js
@@ -0,0 +1,4 @@
+/*
+첫번째와 마지막 구매 항목을 출력
+firstChild, firstElementChilde, lastChild, lastElementChild: 자식요소 접근
+*/
