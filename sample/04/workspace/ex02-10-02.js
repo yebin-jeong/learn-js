b9f87a2db56c75c5d8dcabe7c70791e0516ec3b9
Kilyong Jeong <uzoolove@gmail.com>
2025-04-21 09:31:36 +0900

    🧪 2강 실습 샘플 추가

---
+++ sample/04/workspace/ex02-10-02.js
@@ -0,0 +1,21 @@
+/*
+  함수 생성 4 (화살표 함수 표현식)
+*/
+
+let arr = [10, 20, 30];
+let result = [];
+
+// 각 요소의 제곱값으로 구성된 새로운 배열 생성
+
+
+// Array.prototype.forEach()
+
+
+// Array.prototype.map()
+
+
+// ECMA6 arrow function
+
+
+
+console.log(arr);
