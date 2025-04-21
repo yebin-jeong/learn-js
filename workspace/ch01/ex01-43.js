0cfda3393930751233e6e38e0480fb7b80d260db
yebin-jeong <2022112390@dgu.ac.kr>
2025-04-21 16:15:02 +0900

    매개변수

--- workspace/ch01/ex01-43.js
+++ workspace/ch01/ex01-43.js
@@ -2,5 +2,15 @@
 나머지 매개변수(Rest parameters)
 */
 
+function fn(...args) {
+  console.log(args);
+  for (let arg of args) {
+    console.log(arg);
+  }
+}
 
-
+fn();
+fn(1);
+fn(1, 2);
+fn(1, 2, 3);
+fn(1, 2, 5, 465, 324654, 34654, 65465321, 654, 21354);
--- workspace/ch02/ex02-13.js
+++ workspace/ch02/ex02-13.js
@@ -9,20 +9,37 @@
   - 배열과 비슷하게 length 속성과 index로 각 인자에 접근 가능
 */
 
-function fn(n1, n2){
+function fn(n1, n2) {
   // console.log(n1, n2);
   // 모든 인자값의 합계를 출력
-  var sum = 0;
-  for(var i=0; i<arguments.length; i++){
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
     sum += arguments[i];
   }
-  console.log('합계', sum);
+  console.log("합계", sum);
 }
 
 fn();
 fn(10);
 fn(20, 30);
 fn(40, 50, 60);
-fn(1342,53,64,57,58,67,967,6734,5,234,23,6,467,567,97,8,566,34);
-
-
+fn(
+  1342,
+  53,
+  64,
+  57,
+  58,
+  67,
+  967,
+  6734,
+  5,
+  234,
+  23,
+  6,
+  467,
+  567,
+  97,
+  8,
+  566,
+  34
+);
