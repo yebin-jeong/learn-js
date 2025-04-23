46b00fb5a5a3d6d61452171afa2fc47d45fdf4de
Kilyong Jeong <uzoolove@gmail.com>
2025-04-23 14:20:44 +0900

    ✨ 메모이제이션

--- workspace-ins/ch02/ex02-23.js
+++ workspace-ins/ch02/ex02-23.js
@@ -1,5 +1,5 @@
 // 지정한 수가 소수인지 여부를 반환
-const isPrime = function(num){
+const isPrime = function(num){ // num = 5
   // 캐시를 위한 코드
   isPrime._cache = isPrime._cache || {};
   if(isPrime._cache[num] !== undefined){ // num에 대해서 계산이 끝나고 캐시된 경우
@@ -9,7 +9,7 @@ const isPrime = function(num){
   // 소수 판별 코드
   let prime = true;
 
-  for(let i=2; i<num; i++){
+  for(let i=2; i<=Math.sqrt(num); i++){
     if(num % i === 0){
       prime = false;
       break;
@@ -17,8 +17,8 @@ const isPrime = function(num){
   }
 
   // 캐시를 위한 코드
-  isPrime._cache[num] = prime;
-
+  isPrime._cache[num] = prime; // isPrime._cache[5] = true;
+  
   return prime;
 };
 
@@ -34,3 +34,4 @@ console.log('1000000007 -> ', isPrime(1000000007));
 console.log('1000000007 -> ', isPrime(1000000007));
 console.log('1000000007 -> ', isPrime(1000000007));
 console.timeEnd('소요시간');
+
