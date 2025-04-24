ad2d6ae3448f1ea457f381c59ebb0f6a31c9ef41
yebin-jeong <2022112390@dgu.ac.kr>
2025-04-24 17:55:03 +0900

    that 변수로 this

--- workspace/ch02/ex02-17-01.js
+++ workspace/ch02/ex02-17-01.js
@@ -1,12 +1,12 @@
 /*
- 카운터 예제
+카운터 예제. that 변수로 외부의 this 참조
 */
 
 var count = 0;
 const myObj = {
   count: 0,
   visit: function () {
-    // 방문자를 한 명 증가시킨다.
+    // 방문자를 한명 증가시킨다.
     this.count++; // this = myObj
     const that = this; // that = this = myObj
     const visit2 = function () {
@@ -16,7 +16,6 @@ const myObj = {
   },
 };
 
+myObj.visit(); // this = myObj
 myObj.visit();
-myObj.visit();
-console.log("myObj", myObj.count);
-console.log("window", count);
+console.log("방문자수", myObj.count); // 4
