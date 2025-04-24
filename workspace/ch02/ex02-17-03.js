860a1690b4e6d0d65c2c44532ffc4d05bef3d09c
yebin-jeong <2022112390@dgu.ac.kr>
2025-04-24 17:55:49 +0900

    화살표함수 this

--- workspace/ch02/ex02-17-03.js
+++ workspace/ch02/ex02-17-03.js
@@ -1,12 +1,12 @@
 /*
- 카운터 예제
+카운터 예제. 화살표 함수는 this가 생성되지 않음
 */
 
 var count = 0;
 const myObj = {
   count: 0,
   visit: function () {
-    // 방문자를 한 명 증가시킨다.
+    // 방문자를 한명 증가시킨다.
     this.count++; // this = myObj
     const visit2 = () => {
       this.count++; // this = myObj
@@ -15,7 +15,6 @@ const myObj = {
   },
 };
 
+myObj.visit(); // this = myObj
 myObj.visit();
-myObj.visit();
-console.log("myObj", myObj.count);
-console.log("window", count);
+console.log("방문자수", myObj.count); // 4
