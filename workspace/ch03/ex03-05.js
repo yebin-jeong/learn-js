2d620d50ef6668bcc1521a102a28885848861b9b
yebin-jeong <2022112390@dgu.ac.kr>
2025-04-24 13:25:53 +0900

    상속, 클래스

--- workspace/ch03/ex03-05.js
+++ workspace/ch03/ex03-05.js
@@ -1,4 +1,69 @@
 ﻿/*
-* 프로토타입 체인을 이용한 상속 기능 구현(중계 함수 추가)
-* ex03-04.js 복사
-*/
+ * 프로토타입 체인을 이용한 상속 기능 구현(중계 함수 추가)
+ * ex03-04.js 복사
+ */
+
+/**
+ * 고등학교 성적관리 생성자 함수(총점과 평균 계산)
+ * @param {number} kor 국어 점수
+ * @param {number} eng 영어 점수
+ */
+function HighSchool(kor, eng) {
+  this.kor = kor;
+  this.eng = eng;
+}
+HighSchool.prototype.sum = function () {
+  return this.kor + this.eng;
+};
+HighSchool.prototype.avg = function () {
+  // 소수 첫째자리에서 반올림한다.
+  return Math.round(this.sum() / 2);
+};
+
+const s1 = new HighSchool(100, 91);
+console.log(s1.sum());
+console.log(s1.avg());
+
+/**
+ * 대학교 성적관리 생성자 함수(총점, 평균과 학점 계산)
+ * @param {number} kor 국어 점수
+ * @param {number} eng 영어 점수
+ */
+function College(kor, eng) {
+  HighSchool.call(this, kor, eng);
+}
+
+inherite(HighSchool, College);
+
+// Child가 Parent를 상속 받는다.
+function inherite(Parent, Child) {
+  // const F = new Function();
+  // F.prototype = Parent.prototype;
+  // Child.prototype = new F();
+
+  // Object.create(): 지정한 prototype 객체를 참조하는 인스턴스 생성
+  Child.prototype = Object.create(Parent.prototype);
+  Child.prototype.constructor = Child;
+}
+
+College.prototype.grade = function () {
+  let level = "F";
+  const avg = this.avg();
+  if (avg >= 90) {
+    level = "A";
+  } else if (avg >= 80) {
+    level = "B";
+  } else if (avg >= 70) {
+    level = "C";
+  } else if (avg >= 60) {
+    level = "D";
+  }
+  return level;
+};
+
+const c1 = new College(80, 99);
+console.log(c1.sum()); // 179
+console.log(c1.avg()); // 89.5
+console.log(c1.grade()); // B
+
+console.log(College.prototype);
--- workspace/ch03/ex03-06.js
+++ workspace/ch03/ex03-06.js
@@ -1,4 +1,67 @@
 /*
-* 클래스 정의와 상속
-* ex03-05.js 복사
-*/
+ * 클래스 정의와 상속
+ * ex03-05.js 복사
+ */
+
+/**
+ * 고등학교 성적관리 생성자 함수(총점과 평균 계산)
+ * @param {number} kor 국어 점수
+ * @param {number} eng 영어 점수
+ */
+
+class HighSchool {
+  constructor(kor, eng) {
+    this.kor = kor;
+    this.eng = eng;
+  }
+  sum() {
+    return this.kor + this.eng;
+  }
+  avg() {
+    // 소수 첫째자리에서 반올림한다.
+    return Math.round(this.sum() / 2);
+  }
+}
+
+const s1 = new HighSchool(100, 91);
+console.log(s1.sum());
+console.log(s1.avg());
+
+/**
+ * 대학교 성적관리 생성자 함수(총점, 평균과 학점 계산)
+ * @param {number} kor 국어 점수
+ * @param {number} eng 영어 점수
+ */
+
+class College extends HighSchool {
+  // constructor(kor, eng){
+  //   super(kor, eng); // 부모(HighSchool)의 생성자 호출
+  // }
+
+  // constructor()를 생략할 경우 자동으로 추가
+  constructor(...args) {
+    super(...args);
+  }
+
+  grade() {
+    let level = "F";
+    const avg = this.avg();
+    if (avg >= 90) {
+      level = "A";
+    } else if (avg >= 80) {
+      level = "B";
+    } else if (avg >= 70) {
+      level = "C";
+    } else if (avg >= 60) {
+      level = "D";
+    }
+    return level;
+  }
+}
+
+const c1 = new College(80, 99);
+console.log(c1.sum()); // 179
+console.log(c1.avg()); // 89.5
+console.log(c1.grade()); // B
+
+console.log(College.prototype);
