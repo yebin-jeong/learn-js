507c2faaef11b2874c3db4b6671cf883e0661cb2
Kilyong Jeong <uzoolove@gmail.com>
2025-04-24 16:13:48 +0900

    ♻️

--- workspace-ins/ch04/ex04-05.js
+++ workspace-ins/ch04/ex04-05.js
@@ -24,12 +24,20 @@ const isPrime = function(num){ // num = 5
   return prime;
 };
 
+const sayHello = function(name){
+  return 'Hello ' + name; 
+}
+
+console.log(sayHello('안캐시핑'));
+console.log(sayHello.memo('캐시핑'));
+console.log(sayHello.memo('캐시핑'));
+
 console.time('소요시간');
 console.log('3 -> ', isPrime(3));
 console.log('4 -> ', isPrime(4));
 console.log('5 -> ', isPrime(5));
 console.log('6 -> ', isPrime(6));
-console.log('7 -> ', isPrime(7));
+console.log('7 -> ', isPrime.memo(7));
 console.log('8 -> ', isPrime(8));
 console.log('9 -> ', isPrime(9));
 console.log('1000000007 -> ', isPrime.memo(1000000007));
