32f0d24257ac02505059f808c209dfd4a8a56ee1
Kilyong Jeong <uzoolove@gmail.com>
2025-04-24 14:45:37 +0900

    ✨ 커링

--- workspace-ins/ch04/ex04-03.js
+++ workspace-ins/ch04/ex04-03.js
@@ -2,3 +2,13 @@ function sum(a, b, c){
   return a + b + c;
 }
 console.log(sum(10, 20, 30));
+
+console.log(currySum(10)(20)(30));
+
+function currySum(a){
+  return function(b){
+    return function(c){
+      return a + b + c;
+    };
+  };
+}
