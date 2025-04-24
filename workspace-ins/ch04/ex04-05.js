e0fd921e9e825fe46d21a22f04e0c20dc3f47ccd
Kilyong Jeong <uzoolove@gmail.com>
2025-04-24 15:43:58 +0900

    ✨ 메모이제이션 추가

--- workspace-ins/ch04/ex04-05.js
+++ workspace-ins/ch04/ex04-05.js
@@ -1,1 +1,39 @@
+// ex02-23.js 복사
+
+// 함수에 메모이제이션 기능 추가
+Function.prototype.memo = function(key){ // this = isPrime
+  // 캐시를 위한 코드
+  this._cache = this._cache || {};
+  if(this._cache[key] !== undefined){ // key에 대해서 계산이 끝나고 캐시된 경우
+    return this._cache[key];
+  }else{
+    return this._cache[num] = this(key);
+  }
+};
+
+// 지정한 수가 소수인지 여부를 반환
+const isPrime = function(num){ // num = 5
+  // 소수 판별 코드
+  let prime = true;
+  for(let i=2; i<=num; i++){
+    if(num % i === 0){
+      prime = false;
+      break;
+    }
+  }
+  return prime;
+};
+
+console.time('소요시간');
+console.log('3 -> ', isPrime(3));
+console.log('4 -> ', isPrime(4));
+console.log('5 -> ', isPrime(5));
+console.log('6 -> ', isPrime(6));
+console.log('7 -> ', isPrime(7));
+console.log('8 -> ', isPrime(8));
+console.log('9 -> ', isPrime(9));
+console.log('1000000007 -> ', isPrime.memo(1000000007));
+console.log('1000000007 -> ', isPrime.memo(1000000007));
+console.log('1000000007 -> ', isPrime.memo(1000000007));
+console.timeEnd('소요시간');
 
