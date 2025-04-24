7dae0e15f21a5228fabeb9b3c7cb2c194a8dec9f
Kilyong Jeong <uzoolove@gmail.com>
2025-04-24 14:04:23 +0900

    ♻️

--- workspace-ins/ch04/ex04-01.js
+++ workspace-ins/ch04/ex04-01.js
@@ -4,10 +4,15 @@ function outer(){
   const innerVal = 'outer의 지역변수';
   console.log(topLevel); // 하위 Scope에서 접근 가능
   console.log(innerVal); // 지역변수는 선언한 함수내에서만 접근 가능
-
+  
+  const fn = function(){
+    console.log(innerVal);
+  };
+  
+  return fn;
 }
 
-outer();
+const fn = outer();
 console.log(topLevel); // 같은 Scope 내에서 접근 가능
-console.log(innerVal); // 지역변수는 선언한 함수내에서만 접근 가능
-
+// console.log(innerVal); // 지역변수는 선언한 함수내에서만 접근 가능
+fn();
