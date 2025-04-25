370374eb4dc7a9aab1675f7ab4a988a765dda6d8
Kilyong Jeong <uzoolove@gmail.com>
2025-04-25 17:04:30 +0900

    🧪 병아리 day01

---
+++ sample/progrmmers/daily/day01/05.js
@@ -0,0 +1,22 @@
+/*
+분류: 출력
+문제: 특수문자 출력하기
+url: https://school.programmers.co.kr/learn/courses/30/lessons/181948?language=javascript
+
+문제 설명
+다음과 같이 출력하도록 코드를 작성해 주세요.
+
+출력 예시
+!@#$%^&*(\'"<>?:;
+
+*/
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('close', function () {
+  
+});
