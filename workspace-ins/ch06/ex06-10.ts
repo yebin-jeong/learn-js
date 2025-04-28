462ed6b9203790196c8cb4d83a59c27f1845be65
Kilyong Jeong <uzoolove@gmail.com>
2025-04-28 15:25:48 +0900

    ✨

--- workspace-ins/ch06/ex06-10.ts
+++ workspace-ins/ch06/ex06-10.ts
@@ -6,8 +6,9 @@
   interface User {
     name: string;
     age: number;
-  };
+  }
 
+  // 인터페이스를 객체의 타입을 지정
   const u1: User = {
     name: 'haru',
     age: 5
@@ -16,6 +17,25 @@
     name: 'namu',
     age: 8
   };
-  
   console.log(u1.age, u2.name.toUpperCase());
+
+  // 함수의 리턴 타입으로 인터페이스 지정
+  const createUser = function(name: string, age: number): User {
+    const user = {
+      name: name,
+      age: age,
+    };
+    return user;
+    // return { name, age };
+  }
+  const u3: User = createUser('유저핑', 9);
+  console.log(u3.name, u3.age);
+  
+  // 함수의 매개변수의 타입으로 인터페이스 지정
+  function getAge(user: User): number {
+    return user.age;
+  }
+  console.log(getAge(u1));
+  console.log(getAge(u2));
+  
 })();
--- workspace-ins/ch06/ex06-11.ts
+++ workspace-ins/ch06/ex06-11.ts
@@ -1,33 +1,51 @@
-// // 인터페이스 사용 - 클래스의 타입 지정
+// 인터페이스 사용 - 클래스의 타입 지정
 
-// (()=>{
+(()=>{
 
-//   // 자바스크립트 클래스 선언
-//   class HighSchool {
-//     constructor(kor, eng) {
-//       this.kor = kor;
-//       this.eng = eng;
-//     }
-//     sum() {
-//       return this.kor + this.eng;
-//     }
-//     avg() {
-//       return this.sum() / 2;
-//     }
-//   }
+  // 자바스크립트 클래스 선언
+  // class HighSchool {
+  //   constructor(kor, eng) {
+  //     this.kor = kor;
+  //     this.eng = eng;
+  //   }
+  //   sum() {
+  //     return this.kor + this.eng;
+  //   }
+  //   avg() {
+  //     return this.sum() / 2;
+  //   }
+  // }
 
-//   // FIXME HighSchool 클래스가 구현해야 하는 Score 인터페이스 선언
-//   interface Score {}
+  // FIXME HighSchool 클래스가 구현해야 하는 Score 인터페이스 선언
+  interface Score {
+    kor: number;
+    eng: number;
+    sum(): number;
+    avg(): number;
+  }
 
-//   // FIXME 타입 스크립트 클래스 선언(인터페이스 지정)
-//   class HighSchool {}
+  // FIXME 타입 스크립트 클래스 선언(인터페이스 지정)
+  class HighSchool {
+    kor: number;
+    eng: number;
+    constructor(kor: number, eng: number) {
+      this.kor = kor;
+      this.eng = eng;
+    }
+    sum(): number {
+      return this.kor + this.eng;
+    }
+    avg() { // 타입을 생략할 경우 타입 추론에 의해서 리턴값을 파악하고 적절한 타입을 추론
+      return this.sum() / 2;
+    }
+  }
   
-//   // 총점과 평균을 출력하는 함수
-//   function printScore(score) {
-//     console.log(score.sum(), score.avg());
-//   }
+  // 총점과 평균을 출력하는 함수
+  function printScore(score: Score) {
+    console.log(score.sum(), score.avg());
+  }
 
-//   const haru = new HighSchool(100, 90);
-//   printScore(haru);
+  const haru: Score = new HighSchool(100, 90);
+  printScore(haru);
   
-// })();
+})();
