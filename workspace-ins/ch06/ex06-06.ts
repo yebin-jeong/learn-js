895696879c47c2d96822f27b7b24995a249e5f32
Kilyong Jeong <uzoolove@gmail.com>
2025-04-28 13:15:26 +0900

    ✨

--- workspace-ins/ch06/ex06-06.ts
+++ workspace-ins/ch06/ex06-06.ts
@@ -1,19 +1,19 @@
-// // 유니언 타입
+// 유니언 타입
 
-// (()=>{
+(()=>{
 
-//   // FIXME 타입 에러가 발생하지 않도록 함수의 타입을 지정하세요.
-//   function logMessage(msg) {
-//     console.log(msg);
-//   }
+  // FIXME 타입 에러가 발생하지 않도록 함수의 타입을 지정하세요.
+  function logMessage(msg: string | number) {
+    console.log(msg);
+  }
 
-//   function warnMessage(msg) {
-//     // 스타일 적용
-//     console.log(`%c${msg}`, 'color: yellow; font-size: 20px;');
-//   }
+  function warnMessage(msg: number | string) {
+    // 스타일 적용
+    console.log(`%c${msg}`, 'color: yellow; font-size: 20px;');
+  }
 
-//   logMessage('hello');
-//   logMessage(100);
-//   warnMessage('bye');
-//   warnMessage(200);
-// })();
+  logMessage('hello');
+  logMessage(100);
+  warnMessage('bye');
+  warnMessage(200);
+})();
