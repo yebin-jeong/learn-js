6e8baf75f162503b592aad808cded54a1cb574a9
yebin-jeong <2022112390@dgu.ac.kr>
2025-04-28 17:15:42 +0900

    소문자 대문자 변환 | 병아리반

--- workspace/progrmmers/daily/day01/04.js
+++ workspace/progrmmers/daily/day01/04.js
@@ -18,16 +18,26 @@ aBcDeFg
 AbCdEfG
 */
 
-const readline = require('readline');
+const readline = require("readline");
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
 let input = [];
 
-rl.on('line', function (line) {
-    input = [line];
-}).on('close',function(){
-    str = input[0];
+rl.on("line", function (line) {
+  input = [line];
+}).on("close", function () {
+  str = input[0];
+
+  let res = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i].toUpperCase() === str[i]) {
+      res += str[i].toLowerCase();
+    } else {
+      res += str[i].toUpperCase();
+    }
+  }
+  console.log(res);
 });
