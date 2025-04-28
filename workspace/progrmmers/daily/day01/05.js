424028ebbc6f9d2653593a7c034bb3500bc88edb
yebin-jeong <2022112390@dgu.ac.kr>
2025-04-28 17:45:06 +0900

    문자출력 | 병아리반

--- workspace/progrmmers/daily/day01/05.js
+++ workspace/progrmmers/daily/day01/05.js
@@ -11,12 +11,12 @@ url: https://school.programmers.co.kr/learn/courses/30/lessons/181948?language=j
 
 */
 
-const readline = require('readline');
+const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-rl.on('close', function () {
-  
+rl.on("close", function () {
+  console.log("!@#$%^&*(\\'\"<>?:;");
 });
