7e76c99ef10ed2052e9d047861f9ae0a12f019ea
Kilyong Jeong <uzoolove@gmail.com>
2025-04-28 17:43:21 +0900

    ✨  완료

--- workspace-ins/programmers/daily/day01/04.js
+++ workspace-ins/programmers/daily/day01/04.js
@@ -4,7 +4,8 @@
 url: https://school.programmers.co.kr/learn/courses/30/lessons/181949?language=javascript
 
 문제 설명
-영어 알파벳으로 이루어진 문자열 str이 주어집니다. 각 알파벳을 대문자는 소문자로 소문자는 대문자로 변환해서 출력하는 코드를 작성해 보세요.
+영어 알파벳으로 이루어진 문자열 str이 주어집니다. 
+각 알파벳을 대문자는 소문자로 소문자는 대문자로 변환해서 출력하는 코드를 작성해 보세요.
 
 제한사항
 1 ≤ str의 길이 ≤ 20
@@ -20,14 +21,28 @@ AbCdEfG
 
 const readline = require('readline');
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
 let input = [];
 
 rl.on('line', function (line) {
-    input = [line];
+  input = [line];
 }).on('close',function(){
-    str = input[0];
+  str = input[0];
+  // console.log(str);
+  // console.log(str.length);
+  // console.log(str[0]);
+
+  // aBcDeFg
+  let result = '';
+  for(let i=0; i<str.length; i++){ // i=0, 1, 2, 3, 4, 5, 6
+    if(str[i].toUpperCase() === str[i]) { // 대문자면 소문자로
+      result = result + str[i].toLowerCase(); // Ab, AbCd, AbCdEf
+    }else{ // 소문자면 대문자로
+      result += str[i].toUpperCase(); // A, AbC, AbCdE, AbCdEfG
+    }
+  }
+  console.log(result);
 });
