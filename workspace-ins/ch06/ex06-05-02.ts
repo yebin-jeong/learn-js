80282e5f1160c82fdd1de304900a1a942ed27022
Kilyong Jeong <uzoolove@gmail.com>
2025-04-28 13:08:30 +0900

    📝

--- workspace-ins/ch06/ex06-05-02.ts
+++ workspace-ins/ch06/ex06-05-02.ts
@@ -4,17 +4,17 @@
   // FIXME: print 함수의 타입을 정의하고 msg의 타입에 따라서 결과값 출력
   function print(msg: string | number | boolean) {
     // string일 경우
-    if(typeof msg === 'string'){
+    if(typeof msg === 'string'){ // 타입 가드
       console.log(`${msg} 글자수: ${msg.length}`);
-    }    
+    }
 
     // number일 경우
-    if(typeof msg === 'number'){
+    if(typeof msg === 'number'){ // 타입 가드
       console.log(`${msg} + 10 = ${msg + 10}`);
     }
     
     // boolean일 경우
-    if(typeof msg === 'boolean'){
+    if(typeof msg === 'boolean'){ // 타입 가드
       console.log(`${msg}: ${msg?'참':'거짓'}`);
     }
     
