c578119403636e8d7603b38879574611bad4e9eb
Kilyong Jeong <uzoolove@gmail.com>
2025-04-28 13:52:09 +0900

    🚧

--- workspace-ins/ch06/ex06-07.ts
+++ workspace-ins/ch06/ex06-07.ts
@@ -1,5 +1,20 @@
 // 타입 별칭으로 객체의 타입 선언
 
 (()=>{
+  // 타입 별칭 생성
+  type User = {
+    name: string;
+    age: number;
+  };
+
+  const u1: User = {
+    name: 'haru',
+    age: 5
+  };
+  const u2: User = {
+    name: 'namu',
+    age: 8
+  };
   
+  console.log(u1.age, u2.name.toUpperCase());
 })();
