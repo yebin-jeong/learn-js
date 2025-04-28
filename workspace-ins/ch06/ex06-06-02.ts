7b430e787809bc76ec9c8570de4fccb72b85be35
Kilyong Jeong <uzoolove@gmail.com>
2025-04-28 13:17:45 +0900

    ✨ 타입 별칭 사용

--- workspace-ins/ch06/ex06-06-02.ts
+++ workspace-ins/ch06/ex06-06-02.ts
@@ -2,12 +2,16 @@
 // ex06-06.ts 복사
 
 (()=>{
+
+  // 타입 별칭 추가
+  type Message = string | number;
+
   // FIXME 타입 에러가 발생하지 않도록 함수의 타입을 지정하세요.
-  function logMessage(msg: string | number) {
+  function logMessage(msg: Message) {
     console.log(msg);
   }
 
-  function warnMessage(msg: number | string) {
+  function warnMessage(msg: Message) {
     // 스타일 적용
     console.log(`%c${msg}`, 'color: yellow; font-size: 20px;');
   }
@@ -16,5 +20,5 @@
   logMessage(100);
   warnMessage('bye');
   warnMessage(200);
-  
+
 })();
