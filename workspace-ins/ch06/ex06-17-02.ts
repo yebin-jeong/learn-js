ff365f08008d6b7510d641434d8cac11bc5538b1
Kilyong Jeong <uzoolove@gmail.com>
2025-04-29 11:47:29 +0900

    ✨

--- workspace-ins/ch06/ex06-17-02.ts
+++ workspace-ins/ch06/ex06-17-02.ts
@@ -2,5 +2,13 @@
 // ex06-17.ts 복사
 
 (()=>{
-  
+
+  // T 뿐만 아니라 식별자 규칙을 따르는 아무 문자나 사용 가능하지만 관례적으로 Type의 첫글자인 T를 사용
+  function echo<T>(msg: T): T {
+    return msg;
+  }
+
+  console.log(echo<string>('hello'));
+  console.log(echo<number>(100));
+  console.log(echo<boolean>(true));
 })();
