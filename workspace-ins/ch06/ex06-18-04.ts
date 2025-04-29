6fc07d27884581dcc7879ff295bed6e895ae9e0a
Kilyong Jeong <uzoolove@gmail.com>
2025-04-29 14:05:38 +0900

    ✨ DropdownItem 로 통합

--- workspace-ins/ch06/ex06-18-04.ts
+++ workspace-ins/ch06/ex06-18-04.ts
@@ -2,5 +2,35 @@
 // ex06-18-03.ts 복사
 
 (()=>{
-  
+  interface DropdownItem {
+    value: string | number;
+    selected?: boolean;
+  }
+
+  const cityList: DropdownItem[] = [
+    { value: 'Seoul', selected: false },
+    { value: 'busan' },
+    { value: 'GwangJu', selected: true },
+  ];
+
+  const zipcodeList: DropdownItem[] = [
+    { value: 12345, selected: false },
+    { value: 34567, selected: true },
+    { value: 56789 },
+  ];
+
+  function createDropdownList(list: DropdownItem[]){
+    let value;
+    let options = '';
+
+    for(let item of list) {
+      value = item.value;
+      options += `\t<option selected="${item.selected?'selected':''}">${typeof value==='string'?value.toUpperCase():value}</option>\n`;
+    }
+    
+    return `<select>\n${ options }</select>`;
+  }
+
+  console.log(createDropdownList(cityList));
+  console.log(createDropdownList(zipcodeList));
 })();
