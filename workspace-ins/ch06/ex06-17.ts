962539e266a5600a854fae8176d2a30f950791e7
Kilyong Jeong <uzoolove@gmail.com>
2025-04-29 11:19:08 +0900

    🧪

--- workspace-ins/ch06/ex06-17.ts
+++ workspace-ins/ch06/ex06-17.ts
@@ -1,20 +1,20 @@
-// // echo 함수 - 일반 함수
+// echo 함수 - 일반 함수
 
-// (()=>{
-//   // FIXME string 타입 매개변수를 받고 string 타입을 리턴하는 함수의 타입 지정
-//   function echoString(msg) {
-//     return msg;
-//   }
-//   // FIXME number 타입 매개변수를 받고 number 타입을 리턴하는 함수의 타입 지정
-//   function echoNumber(msg) {
-//     return msg;
-//   }
-//   // FIXMEboolean 타입 매개변수를 받고 boolean 타입을 리턴하는 함수의 타입 지정
-//   function echoBoolean(msg) {
-//     return msg;
-//   }
+(()=>{
+  // FIXME string 타입 매개변수를 받고 string 타입을 리턴하는 함수의 타입 지정
+  function echoString(msg: string): string {
+    return msg;
+  }
+  // FIXME number 타입 매개변수를 받고 number 타입을 리턴하는 함수의 타입 지정
+  function echoNumber(msg: number): number {
+    return msg;
+  }
+  // FIXME boolean 타입 매개변수를 받고 boolean 타입을 리턴하는 함수의 타입 지정
+  function echoBoolean(msg: boolean): boolean {
+    return msg;
+  }
 
-//   console.log(echoString('hello'));
-//   console.log(echoNumber(100));
-//   console.log(echoBoolean(true));
-// })();
+  console.log(echoString('hello'));
+  console.log(echoNumber(100));
+  console.log(echoBoolean(true));
+})();
