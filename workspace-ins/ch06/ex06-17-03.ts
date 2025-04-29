dcdec7744d2370e953d202445358b5678bdc4f65
Kilyong Jeong <uzoolove@gmail.com>
2025-04-29 11:47:33 +0900

    🚧

--- workspace-ins/ch06/ex06-17-03.ts
+++ workspace-ins/ch06/ex06-17-03.ts
@@ -2,5 +2,11 @@
 // ex06-17-02.ts 복사
 
 (()=>{
-  
+  function echo<T>(msg: T): T {
+    return msg;
+  }
+
+  console.log(echo<string>('hello'));
+  console.log(echo<number>(100));
+  console.log(echo<boolean>(true));
 })();
