0d37e5420c33f3c71b91b48855729b4d45d39226
Kilyong Jeong <uzoolove@gmail.com>
2025-04-29 14:35:57 +0900

    ✨ // 변수의 타입 추론

--- workspace-ins/ch06/ex06-19.ts
+++ workspace-ins/ch06/ex06-19.ts
@@ -1,5 +1,15 @@
 // 변수의 타입 추론
 
 (()=>{
-  
+  // 기본 데이터 타입의 변수 타입을 추론에 의지(권장)
+  let name = '이일구'; // string으로 타입 추론
+  // name = 219;
+
+  const age = 20; // number로 타입 추론
+
+  let name2; // any로 타입 추론
+  name2 = '이일구';
+  name2 = 219;
+
+  console.log(name, age, name2);
 })();
