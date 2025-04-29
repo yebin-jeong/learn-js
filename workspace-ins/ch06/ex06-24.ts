aa81ba5c985ea6f89a498cb3f4943b29acdad304
Kilyong Jeong <uzoolove@gmail.com>
2025-04-29 16:42:21 +0900

    ✨

--- workspace-ins/ch06/ex06-24.ts
+++ workspace-ins/ch06/ex06-24.ts
@@ -5,11 +5,11 @@
   const a = document.querySelector('a[href="ch06/ex06-24.js"]');
 
   // FIXME 타입 단언을 사용해서 타입 에러를 없애세요.
-  // if(a !== null){
-  //   a.textContent += ' 클릭';
-  // }
+  if(a !== null){
+    a.textContent += ' 클릭';
+  }
 
   // Non-null assertion(!): TS 문법. 지정한 객체가 null이나 undefined가 아님을 단언
-  a!.textContent += ' 클릭';
+  // a!.textContent += ' 클릭';
 
 })();
