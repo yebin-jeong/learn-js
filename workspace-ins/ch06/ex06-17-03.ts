7040c3fe63b220a66d851ed8c50d1fe0dd1e8a45
Kilyong Jeong <uzoolove@gmail.com>
2025-04-29 11:51:41 +0900

    ✨ 제네릭 타입 제약

--- workspace-ins/ch06/ex06-17-03.ts
+++ workspace-ins/ch06/ex06-17-03.ts
@@ -2,11 +2,16 @@
 // ex06-17-02.ts 복사
 
 (()=>{
-  function echo<T>(msg: T): T {
+  function echo<T extends string | number>(msg: T): T {
     return msg;
   }
 
   console.log(echo<string>('hello'));
   console.log(echo<number>(100));
-  console.log(echo<boolean>(true));
+  // console.log(echo<boolean>(true)); // 타입 에러
+
+  const str = echo<string>('hello');
+  const num = echo<number>(123.456);
+
+  console.log(str.toUpperCase(), num.toFixed(2));
 })();
