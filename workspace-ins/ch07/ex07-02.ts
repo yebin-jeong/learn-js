9a005fa94ec2e6a5f25d8a12eb0ec67573dc4d8f
Kilyong Jeong <uzoolove@gmail.com>
2025-04-30 10:31:23 +0900

    ✨ setTimeout

--- workspace-ins/ch07/ex07-02.ts
+++ workspace-ins/ch07/ex07-02.ts
@@ -4,9 +4,14 @@
 
   console.log('시작');
 
-
+  const timerId = setTimeout((a: number, b: number) => {
+    console.log('3초 후에 실행됩니다.', a + b);
+  }, 1000*3, 10, 20);
 
   console.log('종료');
+
+  // timeout 작업 취소
+  clearTimeout(timerId);
   
 })();
 
