7b118a07cffc50ad29841964cde19fcada764aa2
Kilyong Jeong <uzoolove@gmail.com>
2025-04-30 17:01:01 +0900

    ✨ 배열 메서드

--- workspace-ins/ch07/ex07-15.ts
+++ workspace-ins/ch07/ex07-15.ts
@@ -5,11 +5,11 @@
   const arr1 = ['한놈', '두식이', '석삼', '너구리', '오징어', '육개장', '칠뜨기'];
 
   // 인덱스 1부터 3 앞까지 복사
-  let arr2 = arr1.slice(1, 3);
+  let arr2 = arr1.slice(1, 3); // 1 <= i < 3
   console.log(arr2); // ['두식이', '석삼']
 
   // 인덱스 2부터 2 앞까지 복사
-  arr2 = arr1.slice(2, 2);
+  arr2 = arr1.slice(2, 2); // 2 <= i < 2
   console.log(arr2); // []
 
   // 인덱스 5부터 끝까지 복사
--- workspace-ins/ch07/ex07-18.ts
+++ workspace-ins/ch07/ex07-18.ts
@@ -1,13 +1,13 @@
-// // 내장 객체 Array - some(), every()
+// 내장 객체 Array - some(), every()
 
-// (()=>{
+(()=>{
   
-//   const arr = [1, 2, 3];
-//   // 2의 배수가 하나라도 있나?
-//   const hasEven = arr.some(); // true
-//   // 전부 2의 배수인가?
-//   const allEven = arr.every(); // false
+  const arr = [1, 2, 3];
+  // 2의 배수가 하나라도 있나?
+  const hasEven = arr.some(num => num % 2 === 0); // true
+  // 전부 2의 배수인가?
+  const allEven = arr.every(num => num % 2 === 0); // false
 
-//   console.log(hasEven, allEven);
+  console.log(hasEven, allEven);
 
-// })();
+})();
