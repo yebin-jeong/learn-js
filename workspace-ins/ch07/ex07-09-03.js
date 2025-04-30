90e03399a09c79ad34bf110cce40b8d25d93dedc
Kilyong Jeong <uzoolove@gmail.com>
2025-04-30 15:19:07 +0900

    ✨ dayjs 추가

--- workspace-ins/ch07/ex07-09-03.js
+++ workspace-ins/ch07/ex07-09-03.js
@@ -3,7 +3,25 @@
 
 (()=>{
 
-  
+  const startDate = dayjs('2025-03-27');
+  const finishDate = dayjs('2025-08-08');
+
+  console.log('JS 수업 시작일', getDate(startDate));
+  console.log('수료일', getDate(finishDate));
+
+  const today = dayjs(); // 현재 시간
+  const diffDays = today.diff(startDate, 'day');
+  console.log(`오늘은 수업 시작일로부터 ${diffDays}일째입니다.`);
+
+  const diffFinishDays = finishDate.diff(today, 'day');
+  console.log(`수업 종료일까지 ${diffFinishDays}일 남았습니다.`);
+
+  const hundredDaysLater = startDate.add(100, 'day');
+  console.log(`우리 만난지 백일째 되는 날은 ${getDate(hundredDaysLater)}입니다.`);
+
+  function getDate(date) {
+    return date.format('YYYY-MM-DD');
+  }
 
 })();
 
