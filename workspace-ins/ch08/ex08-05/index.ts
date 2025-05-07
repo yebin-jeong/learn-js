a2d7d12edb98148492eca70e1427f3f8819f1384
Kilyong Jeong <uzoolove@gmail.com>
2025-05-07 11:21:37 +0900

    🔧

---
+++ workspace-ins/ch08/ex08-05/index.ts
@@ -0,0 +1,4 @@
+import { plus, minus } from './math.js';
+
+plus(1, 2);
+minus(2, 3);
--- workspace-ins/ch08/ex08-05/math.ts
+++ workspace-ins/ch08/ex08-05/math.ts
@@ -1,9 +1,9 @@
-function plus(a: number, b: number) {
+export function plus(a: number, b: number) {
   console.log(`${a} + ${b} = ${a + b}`);
   return a + b;
 }
 
-function minus(a: number, b: number) {
+export function minus(a: number, b: number) {
   console.log(`${a} - ${b} = ${a - b}`);
   return a - b;
 }
