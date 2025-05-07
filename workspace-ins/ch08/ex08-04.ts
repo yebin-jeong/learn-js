2385b230b29049be536938065065220b04cea9bf
Kilyong Jeong <uzoolove@gmail.com>
2025-05-07 10:59:14 +0900

    ✨

--- workspace-ins/ch08/ex08-04.ts
+++ workspace-ins/ch08/ex08-04.ts
@@ -10,12 +10,21 @@
    * @throws 0으로 나눌 수 없습니다.
    */
   function divide(x: number, y: number) {
+    if(y === 0) {
+      throw new Error('0으로 나눌 수 없습니다.');
+    }
     return x / y;
   }
 
   function f1() {
-    const result1 = divide(10, Math.round(Math.random()));
-    console.log(result1);
+    try {
+      const result1 = divide(10, Math.round(Math.random()));
+      console.log(result1);
+    } catch(err) {
+      if(err instanceof Error) { // 타입 가드
+        console.error('에러 발생.', err.message); // 에러 메시지 출력
+      }
+    }
     
     const result2 = divide(10, 2);
     console.log(result2);
