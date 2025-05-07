9a24db3626ea745dfac596c322dc42e64d3de0e0
yebin-jeong <2022112390@dgu.ac.kr>
2025-05-07 09:23:24 +0900

    수업 준비

---
+++ workspace/ch05/bom/ex05-19.ts
@@ -0,0 +1,3 @@
+// Web Socket API
+
+
---
+++ workspace/ch08/ex08-09.ts
@@ -0,0 +1,27 @@
+// 비동기 함수 - async 사용
+// 비동기 함수의 작업이 완료된 후 실행할 코드가 있으면 async/await 사용
+
+(()=>{
+  function p1(){
+    
+  }
+  function a1(){
+    
+  }
+
+  function p2(){
+    
+  }
+  function a2(){
+    
+  }
+
+  function test(){
+    
+  }
+
+  console.log('1. 작업 시작.');
+  test();
+  console.log('2. 작업 종료. 이 메세지가 f1 작업 완료 보다 먼저 나오면 비동기로 호출된다는 의미');
+
+})();
