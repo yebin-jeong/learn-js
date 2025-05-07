24690e9a6a0ac510e8914548c0c554849c2cdae2
Kilyong Jeong <uzoolove@gmail.com>
2025-05-07 10:38:23 +0900

    ✨

--- workspace-ins/ch08/ex08-02.ts
+++ workspace-ins/ch08/ex08-02.ts
@@ -7,7 +7,7 @@
     const obj = undefined as any;
     // obj.fn();
 
-    const fn = new Function('x', 'y', 'retrun x + y');
+    const fn = new Function('x', 'y', 'retrun x + y'); // SyntaxError 발생
     console.log(fn(10, 20));
   }
 
