649fd3e800127b5e23d448df23f2d5198900a7f2
Kilyong Jeong <uzoolove@gmail.com>
2025-05-07 15:47:39 +0900

    ✨

--- workspace-ins/ch08/ex08-08-03.ts
+++ workspace-ins/ch08/ex08-08-03.ts
@@ -6,5 +6,32 @@
 // ex08-08-02.ts 복사
 
 (()=>{
-  
+  function f1(){ // 비동기 함수
+    return new Promise<string>((resolve, reject) => {
+      const delay = Math.floor(Math.random() * 1000 * 10);
+      console.log(`\t\t4. ${delay}ms 동안 작업중...`);
+
+      setTimeout(() => {
+        if(delay < 5000) {
+          resolve('f1 작업 결과.' + delay);
+        } else {
+          reject('f1 작업 실패 사유.' + delay);
+        }
+      }, delay);
+    });
+  }
+
+  function test(){ // 비동기
+    f1().then(result => {
+      console.log('8. f1 작업이 완료된 후에 호출.', result);
+    }).then(f1).then(result => {
+      console.log('8. f1 작업이 완료된 후에 호출.', result);
+    }).then(f1).then(result => {
+      console.log('8. f1 작업이 완료된 후에 호출.', result);
+    });
+  }
+
+  console.log('1. 작업 시작.');
+  test();
+  console.log('7. 작업 종료.');
 })();
