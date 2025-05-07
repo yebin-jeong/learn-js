acbc9a6aa2f55fb6ab7db1308aef4a8afe74924f
Kilyong Jeong <uzoolove@gmail.com>
2025-05-07 14:02:14 +0900

    🚧

--- workspace-ins/ch08/ex08-06.ts
+++ workspace-ins/ch08/ex08-06.ts
@@ -2,22 +2,22 @@
 
 (()=>{
   function f1(){
-    console.log('f1 호출됨.');
+    console.log('\t\t3. f1 호출됨.');
 
-    console.log(`작업중...`);
+    console.log(`\t\t4. 작업중...`);
 
-    console.log('f1 리턴됨.');
+    console.log('\t\t5. f1 리턴됨.');
   }
 
   function test(){
-    console.log('test 호출됨.');
+    console.log('\t2. test 호출됨.');
     f1();
-    console.log('test 리턴됨');
+    console.log('\t6. test 리턴됨');
   }
 
   // FIXME 프로그램 실행 흐름에 맞춰서 콘솔 출력 메세지 앞에 번호 추가
-  console.log('작업 시작.');
+  console.log('1. 작업 시작.');
   test();
-  console.log('작업 종료.');
+  console.log('7. 작업 종료.');
 })();
 
