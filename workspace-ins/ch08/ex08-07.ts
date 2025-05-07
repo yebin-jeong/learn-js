f1911202be2059c4a4dc627a84fc0d005fe19bc2
Kilyong Jeong <uzoolove@gmail.com>
2025-05-07 14:14:54 +0900

    ✨ 비동기 함수 호출

--- workspace-ins/ch08/ex08-07.ts
+++ workspace-ins/ch08/ex08-07.ts
@@ -3,6 +3,27 @@
 // ex08-06.ts 복사
 
 (()=>{
-  
+  function f1(){ // 비동기 함수
+    console.log('\t\t3. f1 호출됨.');
+    const delay = Math.floor(Math.random() * 1000 * 10);
+    console.log(`\t\t4. ${delay}ms 동안 작업중...`);
+
+    setTimeout(() => {
+      console.log('\t\t f1 작업 완료.', delay);
+    }, delay);
+
+    console.log('\t\t5. f1 리턴됨.');
+  }
+
+  function test(){ // 비동기
+    console.log('\t2. test 호출됨.');
+    f1();
+    f1();
+    console.log('\t6. test 리턴됨');
+  }
+
+  console.log('1. 작업 시작.');
+  test();
+  console.log('7. 작업 종료.');
 })();
 
