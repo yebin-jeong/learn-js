0e78badc9bc477335c6d8dd180ae819d5976ca05
Kilyong Jeong <uzoolove@gmail.com>
2025-05-08 14:50:44 +0900

    .

--- workspace-ins/ch05/ajax/ex05-14.ts
+++ workspace-ins/ch05/ajax/ex05-14.ts
@@ -1,3 +1,24 @@
 // 랜덤 고양이 이미지 가져오기 - axios 라이브러리
 // ex05-13.ts 복사
 
+// axios 타입 추가
+// npm i @types/axios
+
+import { type Cat } from "./types.js";
+import { appendImages } from "./utils.js";
+
+const url = 'https://api.thecatapi.com/v1/images/search';
+
+const btn = document.querySelector('button');
+
+btn?.addEventListener('click', getImages);
+
+async function getImages(){
+  try {
+    const response = await axios.get<Cat[]>(url);
+    const data = response.data;
+    appendImages(data);
+  } catch(err) { // 네트워크 오류
+    console.error('에러 발생', err);
+  }
+};
