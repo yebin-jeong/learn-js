c78350207b4db17ad7800d914e65b192e15e922a
Kilyong Jeong <uzoolove@gmail.com>
2025-05-08 17:42:43 +0900

    🚧

--- workspace-ins/ch05/bom/ex05-17.ts
+++ workspace-ins/ch05/bom/ex05-17.ts
@@ -33,6 +33,8 @@ const myPosition = document.querySelector('#my-position');
 function showPosition(position: Position){
   if(myPosition){
     myPosition.textContent = `${position.lat},${position.lng}(${position.accuracy.toFixed()}m)`;
+    panTo(position);
+    showMarker(position);
   }
 }
 
@@ -44,5 +46,25 @@ const options = { //지도를 생성할 때 필요한 기본 옵션
 };
 
 const map = new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+
+function panTo(position: Position) {
+  // 이동할 위도 경도 위치를 생성합니다 
+  var moveLatLon = new window.kakao.maps.LatLng(position.lat, position.lng);
+  
+  // 지도 중심을 부드럽게 이동시킵니다
+  // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
+  map.panTo(moveLatLon);            
+}  
+
+function showMarker(position: Position){
+  // 지도를 클릭한 위치에 표출할 마커입니다
+  var marker = new window.kakao.maps.Marker({ 
+    // 지도 중심좌표에 마커를 생성합니다 
+    position: new window.kakao.maps.LatLng(position.lat, position.lng)
+  }); 
+  // 지도에 마커를 표시합니다
+  marker.setMap(map);
+}
+
 
 export {}
