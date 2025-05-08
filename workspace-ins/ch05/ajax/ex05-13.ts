80ba640ef02a424eaaa75059d9a8217b2c70046f
Kilyong Jeong <uzoolove@gmail.com>
2025-05-08 13:51:16 +0900

    🚧

--- workspace-ins/ch05/ajax/ex05-13.ts
+++ workspace-ins/ch05/ajax/ex05-13.ts
@@ -1,3 +1,19 @@
 // 랜덤 고양이 이미지 가져오기 - Fetch API 사용
 // ex05-12.ts 복사
 
+import { type Cat } from "./types.js";
+import { appendImages } from "./utils.js";
+
+const url = 'https://api.thecatapi.com/v1/images/search';
+
+const btn = document.querySelector('button');
+
+btn?.addEventListener('click', getImages);
+
+async function getImages(): void{
+
+  const response = await fetch(url);
+  const data: Cat[] = await response.json();
+  appendImages(data);
+  
+};
