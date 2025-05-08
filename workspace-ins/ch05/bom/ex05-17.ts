404f231a426f5ac3c8828a192770ca5c3bee9ad5
Kilyong Jeong <uzoolove@gmail.com>
2025-05-08 17:46:27 +0900

    ✨

--- workspace-ins/ch05/bom/ex05-17.ts
+++ workspace-ins/ch05/bom/ex05-17.ts
@@ -35,6 +35,7 @@ function showPosition(position: Position){
     myPosition.textContent = `${position.lat},${position.lng}(${position.accuracy.toFixed()}m)`;
     panTo(position);
     showMarker(position);
+    showAccuracy(position);
   }
 }
 
@@ -64,6 +65,23 @@ function showMarker(position: Position){
   }); 
   // 지도에 마커를 표시합니다
   marker.setMap(map);
+}
+
+function showAccuracy(position: Position){
+  // 지도에 표시할 원을 생성합니다
+  var circle = new window.kakao.maps.Circle({
+    center : new window.kakao.maps.LatLng(position.lat, position.lng),  // 원의 중심좌표 입니다 
+    radius: position.accuracy, // 미터 단위의 원의 반지름입니다 
+    strokeWeight: 5, // 선의 두께입니다 
+    strokeColor: '#75B8FA', // 선의 색깔입니다
+    strokeOpacity: 1, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+    strokeStyle: 'dashed', // 선의 스타일 입니다
+    fillColor: '#CFE7FF', // 채우기 색깔입니다
+    fillOpacity: 0.7  // 채우기 불투명도 입니다   
+  }); 
+
+  // 지도에 원을 표시합니다 
+  circle.setMap(map); 
 }
 
 
