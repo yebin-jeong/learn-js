39fba3805bee012fbf19721de3d4b918287344fb
Kilyong Jeong <uzoolove@gmail.com>
2025-05-08 16:48:37 +0900

    🚧

--- workspace-ins/ch05/bom/ex05-17.ts
+++ workspace-ins/ch05/bom/ex05-17.ts
@@ -1,2 +1,30 @@
 // Geolocation API
 
+interface Position {
+  lat: number;
+  lng: number;
+  accuracy: number;
+}
+
+const btn = document.querySelector('button');
+
+btn?.addEventListener('click', find);
+
+function find(){
+  navigator.geolocation.getCurrentPosition((currentPosition) => {
+    console.log(currentPosition);
+    const position: Position = {
+      lat: currentPosition.coords.latitude, // 위도
+      lng: currentPosition.coords.longitude, // 경도
+      accuracy: currentPosition.coords.accuracy // 위경도 오차(m)
+    };
+    showPosition(position);
+  });
+}
+
+const myPosition = document.querySelector('#my-position');
+function showPosition(position: Position){
+  if(myPosition){
+    myPosition.textContent = `${position.lat},${position.lng}`;
+  }
+}
