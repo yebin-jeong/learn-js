f175ab0318302fc630678b8f9950622ffa536c96
Kilyong Jeong <uzoolove@gmail.com>
2025-05-08 11:39:17 +0900

    🚧

--- workspace-ins/ch05/ajax/ex05-12.ts
+++ workspace-ins/ch05/ajax/ex05-12.ts
@@ -2,6 +2,24 @@
 
 const url = 'https://api.thecatapi.com/v1/images/search';
 
-function getImages(){
-  
+const btn = document.querySelector('button');
+
+btn?.addEventListener('click', getImages);
+
+function getImages(): void{
+  // Ajax 프로그래밍 순서
+  // 1. XMLHttpRequest 객체 생성
+  const xhr = new XMLHttpRequest();
+
+  // 2. 요청 준비(open)
+  xhr.open('GET', url, false);
+
+  // 4. 응답 데이터 처리
+  xhr.addEventListener('load', function() {
+    const result = this.responseText;
+    console.log(result);
+  });
+
+  // 3. 서버에 요청(send)
+  xhr.send();
 };
