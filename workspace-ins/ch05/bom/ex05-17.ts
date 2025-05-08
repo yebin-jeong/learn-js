461e1db2250313ee105f33c8da4d5a07a6bd0781
Kilyong Jeong <uzoolove@gmail.com>
2025-05-08 17:24:43 +0900

    🚧

--- workspace-ins/ch05/bom/ex05-17.ts
+++ workspace-ins/ch05/bom/ex05-17.ts
@@ -39,8 +39,8 @@ function showPosition(position: Position){
 
 const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
 const options = { //지도를 생성할 때 필요한 기본 옵션
-	center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-	level: 3 //지도의 레벨(확대, 축소 정도)
+	center: new window.kakao.maps.LatLng(37.634259, 126.709894), //지도의 중심좌표.
+	level: 7 //지도의 레벨(확대, 축소 정도)
 };
 
 const map = new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
