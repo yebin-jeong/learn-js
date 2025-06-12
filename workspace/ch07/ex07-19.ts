d4e6832808cd7fbecf2cd17bad33a530d15e0e76
yebin-jeong <2022112390@dgu.ac.kr>
2025-06-12 17:38:10 +0900

    reduce

--- workspace/ch07/ex07-19.ts
+++ workspace/ch07/ex07-19.ts
@@ -1,5 +1,10 @@
 // 내장 객체 Array - reduce()
 
-(()=>{
-  
+(() => {
+  const arr = [1, 2, 3, 4];
+  const sum = arr.reduce((acc, num, index) => {
+    console.log(`${index}=>${acc}+${num} = ${acc + num}`);
+    return acc + num;
+  }, 0);
+  console.log(sum);
 })();
